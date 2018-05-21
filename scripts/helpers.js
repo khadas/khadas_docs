997802bf0b8fe7acc6e8f73de1eec7b2759db9b2
nick <nick@khadas.com>
2018-05-21 18:45:20 +0800

    scripts/helpers.js: update url

--- scripts/helpers.js
+++ scripts/helpers.js
@@ -118,7 +118,7 @@ hexo.extend.helper.register('url_for_lang', function(path) {
 });
 
 hexo.extend.helper.register('raw_link', function(path) {
-  return 'https://github.com/nicknumb/site/edit/master/source/' + path;
+  return 'https://github.com/nicknumb/khadas_docs/edit/master/source/' + path;
 });
 
 hexo.extend.helper.register('page_anchor', function(str) {
