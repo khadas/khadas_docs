0bc678e7da2fb179a37ff26623b43484ede82fa4
Nick Xie <nick@khadas.com>
2019-08-24 18:27:12 +0800

    enable Travis-CI
    Signed-off-by: Nick Xie <nick@khadas.com>

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Decrypt the private key
-openssl aes-256-cbc -K $encrypted_06b8e90ac19b_key -iv $encrypted_06b8e90ac19b_iv -in .travis/ssh_key.enc -out ~/.ssh/id_rsa -d
+openssl aes-256-cbc -K $encrypted_ebeecafcf619_key -iv $encrypted_ebeecafcf619_iv -in .travis/ssh_key.enc -out ~/.ssh/id_rsa -d
 # Set the permission of the key
 chmod 600 ~/.ssh/id_rsa
 # Start SSH agent
@@ -10,9 +10,9 @@ ssh-add ~/.ssh/id_rsa
 # Copy SSH config
 cp .travis/ssh_config ~/.ssh/config
 # Set Git config
-git config --global user.name "Hexo Bot"
-git config --global user.email bot@hexo.io
+git config --global user.name "Khadas Bot"
+git config --global user.email hello@khadas.com
 # Clone the repository
-git clone git@github.com:hexojs/hexojs.github.io.git .deploy_git
+git clone git@github.com:khadas/khadas.github.io.git .deploy_git
 # Deploy to GitHub
 npm run deploy
