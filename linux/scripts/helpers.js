398c2e1576add5d38d731d13feb04784099b7031
Peter Lin <peter.lin@wesion.com>
2022-04-20 10:38:37 +0800

    linux: fix up the problem that the front and rear navigation of the footer is invalid
    Signed-off-by: Peter Lin <peter.lin@wesion.com>

--- linux/scripts/helpers.js
+++ linux/scripts/helpers.js
@@ -70,19 +70,28 @@ hexo.extend.helper.register('page_nav', function() {
   var type = this.page.canonical_path.split('/')[0];
   var sidebar = this.site.data.sidebar[type];
   var path = pathFn.basename(this.path);
+  
   var list = {};
   var prefix = 'sidebar.' + type + '.';
 
   for (var i in sidebar) {
     for (var j in sidebar[i]) {
-      list[sidebar[i][j]] = j;
+      if(Object.prototype.toString.call(sidebar[i][j]) === '[object Object]'){
+        for(var k in sidebar[i][j]){
+          if(sidebar[i][j][k] !== '[object Object]'){
+            list[sidebar[i][j][k]] = k;
+          }
+        }
+      }else{
+        list[sidebar[i][j]] = j;
+      }
     }
   }
 
   var keys = Object.keys(list);
   var index = keys.indexOf(path);
   var result = '';
-
+  
   if (index > 0) {
     result += '<a href="' + keys[index - 1] + '" class="article-footer-prev" title="' + this.__(prefix + list[keys[index - 1]]) + '">'
       + '<i class="fa fa-chevron-left"></i><span>' + this.__('page.prev') + '</span></a>';
